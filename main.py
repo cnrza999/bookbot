a02da7fdc40b556d61be3a9169526cf7d01425d6
cnrza999 <cnrza6011064@gmail.com>
2025-01-24 23:43:42 +0700

    Make a beautiful report

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     count_characters = count_char(text)
-    print(f"{num_words} words found in the document")
-    print(f"{count_characters}")
+    list_dict_count_characters = [{"char":k, "num":v} for k, v in count_characters.items()]
+    list_dict_count_characters.sort(reverse=True, key=sort_on)
     
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print("\n")
+    # Filter only alphabet
+    for character in list_dict_count_characters:
+        if character["char"].isalpha():
+            print(f"The '{character['char']}' character was found {character['num']} times")
+    print("--- End report ---")
+    
+def sort_on(dict):
+    return dict["num"]
 
 def get_num_words(text):
     words = text.split()
